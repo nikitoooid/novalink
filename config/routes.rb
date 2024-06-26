07f726f9a30b1055876fe1bc43836ff2e711de84
nikitoooid <nekytnp@gmail.com>
2024-06-26 13:24:47 +0300

    Link views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  root 'application#index'
 
   resources :links, except: :show
   get '/:slug', to: 'links#redirect', as: :slug_redirect
